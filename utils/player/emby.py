a372957a6de58dd9f160a8bdca2275ab75e02862
hqyz <1134103411@qq.com>
2023-02-11 12:02:07 +0800

    添加播放暂停即限速逻辑

--- utils/player/emby.py
+++ utils/player/emby.py
@@ -13,9 +13,9 @@ def parse_emby_webhooks(context: dict):
         data = context['data']
         item = data[0]
         event = json.loads(str(item))
-        if event['Event'] == "playback.start":
+        if event['Event'] == "playback.start" or event['Event'] == "playback.unpause":
             return Event(EVENT_START, event['Session']['RemoteEndPoint'], True)
-        if event['Event'] == "playback.stop":
+        if event['Event'] == "playback.stop" or event['Event'] == "playback.pause":
             return Event(EVENT_STOP, event['Session']['RemoteEndPoint'], True)
         return Event(EVENT_OTHER, event['Session']['RemoteEndPoint'], True)
     except Exception as e:
@@ -29,8 +29,8 @@ def get_emby_playing_session_count(url, api_key):
         data = r.json()
         count = 0
         for item in data:
-            if "NowPlayingItem" in item and not check_ip_if_internal(item['RemoteEndPoint'],
-                                                                     Config().cfg.limiter.exclude_ip or []):
+            if "NowPlayingItem" in item and not item['PlayState']['IsPaused'] and \
+                    not check_ip_if_internal(item['RemoteEndPoint'], Config().cfg.limiter.exclude_ip or []) :
                 count += 1
                 logger.info("监测到Emby外网用户{}({})正在播放".format(item['UserName'], item['RemoteEndPoint']))
         logger.info("当前Emby外网播放会话数:{}".format(count))
